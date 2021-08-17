bb703965fcb724a9465f1c71b9b747a95f1ad270
Heds Simons <heds@whaleway.net>
2021-08-16 21:26:38 -0700

    Use Otel collector, tidy up stuff, pre-provision data sources.
    Signed-off-by: Heds Simons <heds@whaleway.net>

--- http-requester/tracing.js
+++ http-requester/tracing.js
@@ -1,26 +1,19 @@
-/* tracing.js */
-
-// Require dependencies
-//const { diag, DiagConsoleLogger, DiagLogLevel } = require("@opentelemetry/api");
+// Include all deps
 const api = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/node");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/tracing");
+const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
-
 module.exports = () => {
   const options = {
-    tags: [], // optional
-    // You can use the default UDPSender
-    host: 'tempo', // optional
-    port: 6832, // optional
-    // OR you can use the HTTPSender as follows
-    // endpoint: 'http://localhost:14268/api/traces',
-    maxPacketSize: 65000 // optional
+    tags: [],
+    host: 'otel-collector',
+    port: 6832,
+    maxPacketSize: 65000
   }
 
   // Create a tracer provider
@@ -30,25 +23,20 @@ module.exports = () => {
     }),
   });
 
-  //console.log(api.context.active());
-
-  // The exporter handles sending spans to your tracing backend
-  //const exporter = new ConsoleSpanExporter();
+  // Export to Jaeger
   const exporter = new JaegerExporter(options);
 
-  // The simple span processor sends spans to the exporter as soon as they are ended.
+  // Use simple span (should probably use Batch)
   const processor = new SimpleSpanProcessor(exporter);
   provider.addSpanProcessor(processor);
-
-  // The provider must be registered in order to
-  // be used by the OpenTelemetry API and instrumentations
   provider.register();
 
-  // This will automatically enable all instrumentations
+  // Enable everything that's available
   registerInstrumentations({
     instrumentations: [getNodeAutoInstrumentations()],
   });
 
+  // Return instances of the API and the tracer to the calling app
   return {
     tracer: api.trace.getTracer("test"),
     api: api
--- mythical-creatures/tracing.js
+++ mythical-creatures/tracing.js
@@ -1,46 +1,7 @@
-/* tracing.js */
-
-/*const process = require('process');
-const opentelemetry = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { SpanKind } = require('@opentelemetry/api');
-
-module.exports = () => {
-
-  // configure the SDK to export telemetry data to the console
-  // enable all auto-instrumentations from the meta package
-  const traceExporter = new ConsoleSpanExporter();
-  const sdk = new opentelemetry.NodeSDK({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: 'my-service',
-    }),
-    traceExporter,
-    instrumentations: [getNodeAutoInstrumentations()]
-  });
-
-  // initialize the SDK and register with the OpenTelemetry API
-  // this enables the API to record telemetry
-  sdk.start()
-    .then(() => console.log('Tracing initialized'))
-    .catch((error) => console.log('Error initializing tracing', error));
-
-  console.log(sdk);
-
-  return {
-    tracer: sdk.api.trace.getTracer("test-tracer"),
-    api: sdk.api
-  }
-};*/
-
-
-// Require dependencies
-//const { diag, DiagConsoleLogger, DiagLogLevel } = require("@opentelemetry/api");
+// Include all deps
 const api = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/node");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/tracing");
+const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
@@ -49,13 +10,10 @@ const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventi
 
 module.exports = () => {
   const options = {
-    tags: [], // optional
-    // You can use the default UDPSender
-    host: 'tempo', // optional
-    port: 6832, // optional
-    // OR you can use the HTTPSender as follows
-    // endpoint: 'http://localhost:14268/api/traces',
-    maxPacketSize: 65000 // optional
+    tags: [],
+    host: 'otel-collector',
+    port: 6832,
+    maxPacketSize: 65000
   }
 
   // Create a tracer provider
@@ -65,25 +23,20 @@ module.exports = () => {
     }),
   });
 
-  //console.log(api.context.active());
-
-  // The exporter handles sending spans to your tracing backend
-  //const exporter = new ConsoleSpanExporter();
+  // Export to Jaeger
   const exporter = new JaegerExporter(options);
 
-  // The simple span processor sends spans to the exporter as soon as they are ended.
+  // Use simple span (should probably use Batch)
   const processor = new SimpleSpanProcessor(exporter);
   provider.addSpanProcessor(processor);
-
-  // The provider must be registered in order to
-  // be used by the OpenTelemetry API and instrumentations
   provider.register();
 
-  // This will automatically enable all instrumentations
+  // Enable everything that's available
   registerInstrumentations({
     instrumentations: [getNodeAutoInstrumentations()],
   });
 
+  // Return instances of the API and the tracer to the calling app
   return {
     tracer: api.trace.getTracer("test"),
     api: api
